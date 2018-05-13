6aa751e3d037de886a4a97f644afbb67d141bb8a
awcooper <wesner.cooper@gmail.com>
2018-05-13 15:30:22 -0400

    udpated gitignore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello, world!"
   end
+
+    def goodbye
+    render html: "<h1>goobye cruel world!</h1>"
+  end
 end

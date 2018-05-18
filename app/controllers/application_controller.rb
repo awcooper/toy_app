7ccff022cc954efa00986ed73795697dc81fcabb
awcooper <wesner.cooper@gmail.com>
2018-05-17 20:52:34 -0400

    removed some dead code from previous exercise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  def hello
-    render html: "hello, world!"
-  end
-
-    def goodbye
-    render html: "<h1>goobye cruel world!</h1>"
-  end
 end
